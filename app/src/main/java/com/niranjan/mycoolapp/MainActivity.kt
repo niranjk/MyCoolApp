2d878b4ee071806a325eef550bcb775a5b52f1aa
Niranjan Khatri <niranjan.khatri@nttdata.com>
2020-11-30 13:14:30 +0100

    Data Binding Implementation

--- app/src/main/java/com/niranjan/mycoolapp/MainActivity.kt
+++ app/src/main/java/com/niranjan/mycoolapp/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.niranjan.mycoolapp
 
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.niranjan.mycoolapp.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     // lateinit var button: Button
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         setRandomIconForImageView()
     }
 
     private fun setRandomIconForImageView() {
         // button = findViewById(R.id.generateBtn)
+        // 1. Data Binding
+        binding.floatingActionButton.setOnClickListener {
+            Toast.makeText(this, "Clicked Floating Action Info Button: Data Binding", Toast.LENGTH_LONG).show()
+        }
+
+        // 2. Kotlin Synthetic Library.
         generateBtn.setOnClickListener {
 
             var randomNumber = Random.nextInt(4)+1
